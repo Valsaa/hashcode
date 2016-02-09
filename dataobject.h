081a36b5b9c94435a466a4390f131042c2990e94
lemoissonneur <lemoisso@gmail.com>
2016-02-09 20:57:47 +0100

    maj class.h

--- dataobject.h
+++ dataobject.h
@@ -4,37 +4,51 @@
 #include <iostream>
 #include <cstdio>
 
+#define NB_POOL			45
+#define NB_SERVERS		625
+#define NB_SLOTS		1600
+#define R				16
+#define S				100
+#define NB_SLOT_UNAV	80
+
 typedef enum {FREE, USED, UNAV} etat_slot;
-
-class DataObject {
-
-	public :
-		DataObject(void);
-		int readData(const char *filename);
-		~DataObject(void);
-};
 
 class Slot {
 
 	public :
-		int r;
-		int s;
-		etat_slot dispo;
+		int _r;
+		int _s;
+		int _size;
+		etat_slot _dispo;
 		
 		Slot(int row, int slot, etat_slot state);
+		Slot(int row, int slot, int size);
 };
 
 class Server {
 
 	public :
-		int capacite;
-		int size;
-		int pool;
-		int slot;
+		int _capacite;
+		int _size;
+		int _pool;
+		int _r;
+		int _s;
 		
-		Server(int ca, int si, int po, int sl);
-		
+		Server(int ca, int si, int po);
 		
 };
+
+class DataObject {
+
+	public :
+	
+		Slot *list_slot;
+		Server *list_server;
+		
+		DataObject(void);
+		int readData(const char *filename);
+		~DataObject(void);
+};
+
 
 #endif // DATAOBJECT_H
