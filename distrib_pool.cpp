9f4864993d12bbf487158e05f92696089bd80b35
Valentin Jaouen <valentin.jaouen@supelec.fr>
2016-02-10 00:31:39 +0100

    Boolean power !!!!

--- distrib_pool.cpp
+++ distrib_pool.cpp
@@ -3,11 +3,17 @@
 void distrib_pool(DataObject *data, vector<Server> *serv) {
 	for(int id_pool = 0 ; id_pool < NB_POOL ; id_pool++) {
 		int coef = (int) data->Calcul_CP();
-		while(coef >= 0) {
+		bool boolean = false;
+		while(coef >= 0 && boolean == false) {
 			for(int i = 0 ; i < R ; i++) {
 				vector<Server*> list_serv = data->getServerAtRow(i);
 				for(int j = 0 ; j < list_serv.size() ; j++) {
+					if(list_serv.at(j)->_pool != -1) {
+						boolean = true;
+					}
+
 					if(list_serv.at(j)->_pool == -1 && coef >= 0) {
+						boolean = false;
 						for(int k = 0 ; k < serv->size() ; k++) {
 							if(list_serv.at(j)->_id == serv->at(k)._id) {
 								serv->at(k)._pool = id_pool;
