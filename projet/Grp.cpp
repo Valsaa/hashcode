07628d4da602b6534b51706e0a544e546ecd9a83
lemoissonneur <lemoissonneur@VBox-meca.(none)>
2016-02-11 20:23:13 +0100

    debug

--- projet/Grp.cpp
+++ projet/Grp.cpp
@@ -1,4 +1,6 @@
 #include "Grp.h"
+#include <math.h>
+#include <cstdlib>
 
 Grp::Grp()
 {
@@ -9,15 +11,15 @@ int Grp::calcul_distance_W(Warehouse* WH)
 {
 	int X_bar = 0, Y_bar = 0;
 	
-	for(int a=0 ; a<=cmd.size() ; a++)
+	for(int a=0 ; a<= _cmd->size() ; a++)
 	{
-		X_bar += cmd.at(a)._positionX;
-		Y_bar += cmd.at(a)._positionY;
+		X_bar += _cmd->at(a)->_positionX;
+		Y_bar += _cmd->at(a)->_positionY;
 	}
-	X_bar = X_bar/cmd.size();
-	Y_bar = Y_bar/cmd.size();
+	X_bar = X_bar/_cmd->size();
+	Y_bar = Y_bar/_cmd->size();
 	
-	return ceil( sqrt( (abs(X_bar-WH->_positionX))*(abs(X_bar-WH->_positionX)) + abs(Y_bar-WH->_positionY))*(abs(Y_bar-WH->_positionY)) ) );
+	return ceil( sqrt( (abs(X_bar-WH->_positionX))*(abs(X_bar-WH->_positionX)) + (abs(Y_bar-WH->_positionY))*(abs(Y_bar-WH->_positionY)) ) );
 }
 
 Grp::~Grp()
